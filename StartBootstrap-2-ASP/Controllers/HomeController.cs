e1a1de852eccaa0f9347e7bdcf44167b498f407d
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-11 08:47:31 +0400

    Works are done

---
+++ StartBootstrap-2-ASP/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StartBootstrap_2_ASP.Data;
+using StartBootstrap_2_ASP.Models;
+using StartBootstrap_2_ASP.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartBootstrap_2_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome()
+            {
+                settings = _context.settings.FirstOrDefault(),
+                product = _context.products.ToList(),
+                CartCount = _context.carts.Count()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ StartBootstrap-2-ASP/ViewModel/VmHome.cs
@@ -0,0 +1,12 @@
+﻿using StartBootstrap_2_ASP.Models;
+using System.Collections.Generic;
+
+namespace StartBootstrap_2_ASP.ViewModel
+{
+    public class VmHome
+    {
+        public Settings settings { get; set; }    
+        public List<Product> product { get; set; }   
+        public int CartCount { get; set; }
+    }
+}
